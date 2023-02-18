0cd347e5e3a2fc0017796427fc80f7e6b60f8a57
JoaovlLima <jl949615@gmail.com>
2023-02-17 22:58:15 -0200

    17/02/23

---
+++ Avaliação/App.java
@@ -0,0 +1,8 @@
+package Avaliação;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Avaliação/NotaAlunos.java
@@ -0,0 +1,8 @@
+package Avaliação;
+
+import java.util.Scanner;
+
+public class NotaAlunos {
+    Scanner sc new 
+    
+}
