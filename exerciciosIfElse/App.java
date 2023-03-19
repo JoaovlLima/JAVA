c9cee21a5e6353f6b21d049fb8d9aaf1e296e700
joaovllima <jl949615@gmail.com>
2023-03-19 10:19:41 -0300

    19.03.23

---
+++ exerciciosIfElse/App.java
@@ -0,0 +1,17 @@
+package exerciciosIfElse;
+
+public class App {
+    public static void main(String[] args) {
+        Exercicio1 obj1 = new Exercicio1();
+        obj1.exercicio1();
+        Exercicio1 obj2 = new Exercicio1();
+        obj2.exercicio2();
+        Exercicio1 obj3 = new Exercicio1();
+        obj3.exercicio3();
+        Exercicio1 obj4 = new Exercicio1();
+        obj4.exercicio4();
+        
+
+    }
+    
+}
