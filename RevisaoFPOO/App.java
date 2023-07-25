8378163d0c412530fb25edffa97e58d9c86abd98
JoaovlLima <124844047+JoaovlLima@users.noreply.github.com>
2023-07-25 15:17:56 -0300

    Add files via upload

---
+++ RevisaoFPOO/App.java
@@ -0,0 +1,10 @@
+package RevisaoFPOO;
+
+import RevisaoFPOO.Cadastro.Animais;
+
+public class App {
+    public static void main(String[] args) {
+        Animais obj1 = new Animais("pequeno", 15.55, "Joao", "Femea", "Cachorro");
+
+    }
+}
