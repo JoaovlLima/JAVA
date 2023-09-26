293fb137530ccd9d0b8629fbcdad522129823ced
JoaovlLima <124844047+JoaovlLima@users.noreply.github.com>
2023-09-26 15:02:28 -0300

    Add files via upload

--- ExcExtraGUI/src/App.java
+++ ExcExtraGUI/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
     /*   new Exercicio1(); */
-      new ExemploExercicio1();
+      /* new ExemploExercicio1(); */
+      new Exercicio2();
     }
 }
--- ExcExtraGUI/src/Exercicio2.java
+++ ExcExtraGUI/src/Exercicio2.java
@@ -24,8 +24,51 @@ public class Exercicio2 extends JFrame {
         // Componentes home
         Font fontTitulo = new Font("Serif", Font.PLAIN, 20);
 
+        JPanel mainHome = new JPanel(new BorderLayout());
+        home.add(mainHome);
+
         JLabel tituloHome = new JLabel("SEJA BEM VINDO");
-        tituloHome.
+        tituloHome.setFont(fontTitulo);
+
+        JButton botaoLogin = new JButton("Login");
+        JButton botaoCadastro = new JButton("Cadastro");
+
+        mainHome.add(tituloHome, BorderLayout.NORTH);
+        mainHome.add(botaoLogin, BorderLayout.CENTER);
+        mainHome.add(botaoCadastro, BorderLayout.SOUTH);
+
+        //Componentes Login
+
+        JPanel mainLogin = new JPanel(new BorderLayout());
+        login.add(mainLogin);
+
+        JLabel tituloLogin = new JLabel("FAÇA SEU LOGIN");
+        tituloLogin.setFont(fontTitulo);
+
+        JPanel campoPreenchimento = new JPanel(new GridLayout(2,1));
+
+        JPanel areaNome = new JPanel(new GridLayout(2,1));
+        JPanel areaSenha = new JPanel(new GridLayout(2,1));
+
+        JLabel tituloNome = new JLabel("Nome:");
+        JTextField campoNome = new JTextField();
+
+        JLabel tituloSenha = new JLabel("Senha:");
+        JTextField campoSenha = new JTextField();
+
+        login.add(tituloLogin);
+        
+
+        
+
+       
+        this.add(mainP);
+        this.setDefaultCloseOperation(2);
+        this.setBounds(100, 100, 300, 300);
+        this.setVisible(true);
+
+
+
 
 
 
